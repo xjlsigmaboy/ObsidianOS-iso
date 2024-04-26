0a3d94f09d0e0eafb8ba509db5efad8d3b4fc1ce
Mohamed Tarek <85705200+mohamedtarekxjl@users.noreply.github.com>
2024-04-26 18:11:37 +0300

    Update profiledef.sh

--- baseline/profiledef.sh
+++ baseline/profiledef.sh
@@ -18,4 +18,5 @@ airootfs_image_tool_options=('-zlzma,109' -E 'ztailpacking,fragments,dedupe')
 bootstrap_tarball_compression=(zstd -c -T0 --long -19)
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:400"
 )
